77af1dfb762f998d00367ce1f82796498156f5f6
McNoggins <gagnon88@gmail.com>
2014-10-17 16:22:11 -0400

    Added various files generated by cookiecutter

---
+++ tests/test_latgen.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_latgen
+----------------------------------
+
+Tests for `latgen` module.
+"""
+
+import unittest
+
+from latgen import latgen
+
+
+class TestLatgen(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
