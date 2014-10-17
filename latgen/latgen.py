8264738298f6195b41413426c2d7d4a33f252278
McNoggins <gagnon88@gmail.com>
2014-10-17 16:17:15 -0400

    ENH latgen.py to add honeycomb lattice

--- latgen/latgen.py
+++ latgen/latgen.py
@@ -1,1 +1,75 @@
 # -*- coding: utf-8 -*-
+import numpy as np
+
+def honeycomb(depth=1,pitch=1.0):
+    """Generate the points of a regular honeycomb lattice.
+
+    Keyword arguments:
+    depth -- the depth of the honeycomb lattice, counted in number of
+    points starting from the central point (default 1)
+    
+    pitch -- the lattice pitch (default 1.0)
+    """
+
+    # Initialize some empty lists
+    X=[]
+    Y=[]
+
+    hx=float(pitch)
+    hy=0.5*np.sqrt(3.0)*hx
+
+    # Generate every row using a loop
+    for rowid in range(depth):
+
+        if (rowid % 2) == 0: # Even row
+            for colid in range(depth - rowid/2):
+                # Base point
+                X.extend([colid*hx])
+                Y.extend([rowid*hy])
+
+                # Execute for non-zero rows
+                if (rowid != 0):
+                    # Y-symmetry
+                    X.extend([colid*hx])
+                    Y.extend([-rowid*hy])
+
+                # Execute for non-zero columns
+                if (colid != 0):
+                    # X-symmetry
+                    X.extend([-colid*hx])
+                    Y.extend([rowid*hy])
+
+                # Execute for non-zero rows and columns
+                if (colid != 0 and rowid !=0):
+                    # X-Y symmetry
+                    X.extend([-colid*hx])
+                    Y.extend([-rowid*hy])
+
+
+        elif (rowid % 2) == 1: # Odd row
+            for colid in range(depth-(rowid +1)/2 ):
+                # Base point
+                X.extend([ (0.5+colid)*hx])
+                Y.extend([ rowid*hy])
+
+                # X symmetry
+                X.extend([ -(0.5+colid)*hx])
+                Y.extend([ rowid*hy])
+
+                # Y symmetry
+                X.extend([ (0.5+colid)*hx])
+                Y.extend([ -rowid*hy])
+
+                # X-Y symmetry
+                X.extend([ - (0.5+colid)*hx])
+                Y.extend([ -rowid*hy])
+
+    # Convert to numpy arrays and sort
+    x=np.array(X)
+    y=np.array(Y)
+   
+    xsort=x[np.lexsort((X,Y))]
+    ysort=y[np.lexsort((X,Y))]
+
+    # Return sorted numpy arrays
+    return xsort,ysort
