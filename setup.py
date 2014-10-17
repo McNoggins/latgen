1e53c76201f559b89a3ec20ab53251256d3fc7b7
McNoggins <gagnon88@gmail.com>
2014-10-17 15:57:40 -0400

    Initial commit of local code

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+requirements = [
+    # TODO: put package requirements here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='latgen',
+    version='0.1.0',
+    description='Computational library to generate lattice points',
+    long_description=readme + '\n\n' + history,
+    author='Denis Gagnon',
+    author_email='gagnon88@gmail.com',
+    url='https://github.com/McNoggins/latgen',
+    packages=[
+        'latgen',
+    ],
+    package_dir={'latgen':
+                 'latgen'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD",
+    zip_safe=False,
+    keywords='latgen',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
