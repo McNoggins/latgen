15dda7f3922768b5290cef60cb06c932e6aa5dcd
McNoggins <gagnon88@gmail.com>
2014-10-20 11:16:04 -0400

    ADD __init.py__

--- latgen/__init__.py
+++ latgen/__init__.py
@@ -3,3 +3,12 @@
 __author__ = 'Denis Gagnon'
 __email__ = 'gagnon88@gmail.com'
 __version__ = '0.1.0'
+
+from __future__ import print_function
+try:
+    from .latgen import honeycomb
+except ImportError as e:
+    import sys
+    print('''\
+Could not import submodules (exact error was: {0}).
+'''.format(e), file=sys.stderr)
